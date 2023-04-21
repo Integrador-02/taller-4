f0c1776f4e542838e35cab80ec395a0824ab4725
atardecer01 <rivas.lina@correounivalle.edu.co>
2023-04-20 19:27:12 -0600

    video-aunque aun esta en proceso

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,9 +1,36 @@
 import { OrbitControls, Html, Text,Float } from '@react-three/drei'
 import { Suspense, useRef } from 'react'
+import { FrontSide, LinearFilter, VideoTexture } from 'three'
 
 
 export default function Experience()
 {
+    //Get your video element(traer el elemento video):
+    const video = document.getElementById('video')
+
+    /*
+    video.onloadeddata = function () 
+    {
+        video.play();
+    }*/
+
+    const fun = (ho) =>
+    {
+        if(ho)
+        {
+            video.play();
+        }
+        else{
+            video.pause();
+        }
+    }
+
+    //crear el video como textura:
+    const videoTexture = new VideoTexture(video);
+   /* videoTexture.minFilter = LinearFilter;
+    videoTexture.magFilter = LinearFilter;*/
+    videoTexture.needsUpdate = true;
+
     return <>
 
         <OrbitControls maxPolarAngle = {Math.PI/2} 
@@ -20,7 +47,7 @@ export default function Experience()
         </mesh>
         <mesh position-y={-0.15} position-x={-5} rotation-x={-Math.PI*0.15} rotation-y={-Math.PI*0.5} scale ={10}  >
             <boxGeometry args={[1,1,0.2,2,2,2]} />
-            <meshBasicMaterial color='grey'/>
+            <meshBasicMaterial map = {videoTexture} side={FrontSide} toneMapped={false} needsUpdate={true} onClick = {fun(true)} />
         </mesh>
     </>
 }
